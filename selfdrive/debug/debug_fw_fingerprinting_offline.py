84546dca4dbd6829eb7e3351ac750a7cf1f0c628
Eric Brown <eric@ebrown.net>
2024-12-04 17:07:16 -0700

    Move uds.py to opendbc repo

--- selfdrive/debug/debug_fw_fingerprinting_offline.py
+++ selfdrive/debug/debug_fw_fingerprinting_offline.py
@@ -3,7 +3,7 @@ import argparse
 
 from openpilot.tools.lib.live_logreader import live_logreader
 from openpilot.tools.lib.logreader import LogReader, ReadMode
-from panda.python import uds
+from opendbc.can import uds
 
 
 def main(route: str | None, addrs: list[int]):
